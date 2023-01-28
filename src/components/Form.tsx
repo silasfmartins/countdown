ff720aa6380554cb6c21efd3605780b5c9bfd73b
Silas Martins <silas.martins2041@gmail.com>
2023-01-28 12:06:02 -0300

    Changing Form autofocus

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -13,7 +13,6 @@ export function Form() {
         type="text"
         placeholder="Seu Email"
         className="p-4 rounded-lg mt-3 bg-zinc-100 text-black placeholder:text-zinc-500 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 focus:ring-offset-zinc-100"
-        autoFocus
       />
       <button
         className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 text-white font-semibold bg-primary hover:bg-primary/80 transition-colors focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 focus:ring-offset-zinc-100"
